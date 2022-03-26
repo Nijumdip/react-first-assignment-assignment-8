7af1c3dee1580f8f05bb2aaea5acbbca1534389b
Nijumdip <nijumdip019911@gmail.com>
2022-03-26 19:27:19 +0600

    added 3number question

--- src/Question/Question.jsx
+++ src/Question/Question.jsx
@@ -14,6 +14,27 @@ const Question = () => {
                 <Alert.Heading>Q 2: How useState works ?</Alert.Heading>
                 <p>
                 Ans:
+                useState special function এটাকে hook বলে হয়ে থাকে .যেটা রিয়েক্ট এর কিছু কিছু functionality implement করে দেয় যাতে সে code কে optimize করতে পারে .useState এবং setState উভয়ই Asynchronous . useState ফাংশন দুটি উপাদান সহ একটি array প্রদান করে: একটি value এবং একটি update function।  আপনি যদি variable জন্য একটি প্রাথমিক মান সেট করতে চান, তাহলে useState ফাংশনে একটি argument হিসাবে প্রাথমিক মানটি পাস করুন। যখন রিঅ্যাক্ট প্রথমে কম্পোনেন্ট চালায়, useState যথারীতি দুই-উপাদান array ফিরিয়ে দেবে কিন্তু অ্যারের প্রথম এলিমেন্টে একটি স্থানীয় variable ফেরত দেয় এবং দ্বিতীয় টি pass করা প্যারামিটারে স্থানীয় variable set করে .
+                </p>
+            </p>
+            <p className="mb-0">
+                <Alert.Heading>Q 2: State and Props deference ?</Alert.Heading>
+                <p>
+                Ans:(state)<br></br>
+                i. State changes can be asynchronous <br></br>
+                ii. State can be modified using this .setState. <br></br>
+                iii. It may or may not change <br></br>
+                iv. The place where The state modifies or changes is called the state full component <br></br>
+                v. The state is where the state is declared <br></br>
+ 
+                </p>
+                <p>
+                (Props)<br></br>
+                i.Props are read-only <br></br>
+                ii.Props can not be modified <br></br>
+                iii. It may not changes just read . <br></br>
+                iv. If you want to send, you have to send it through props <br></br>
+                V. The component with the props is called the presentation component <br></br>
                 
                 </p>
             </p>
